8a01dbb9540c2d086c0fdea73d6c28db2e0bf277
wanglikun <523426851@qq.com>
2021-06-24 14:57:55 +0800

    0

---
+++ craco.config.js
@@ -0,0 +1,20 @@
+/* 
+配置自定义主题
+craco.config.js
+*/
+
+const CracoLessPlugin = require('craco-less');
+module.exports = {
+    plugins:[{
+        plugin: CracoLessPlugin,
+        options: {
+          lessLoaderOptions: {
+            lessOptions: {
+              modifyVars: { '@primary-color': '#1890ff' },
+              javascriptEnabled: true,
+            },
+          },
+        },
+      },
+    ]
+};
